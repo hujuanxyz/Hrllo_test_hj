396dbe3a18f3ecbcee3604291e78f6093c7eb0c1
hujuan0703 <hujuan0703@thundersoft.com>
2017-07-28 10:27:13 +0800

    first commit hj

---
+++ src/com/hj/dao/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hj.dao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("helloworld_hj_000");
+	}
+
+}
